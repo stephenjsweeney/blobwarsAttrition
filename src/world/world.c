7598af42a05485eae206b1c1ef5356b87953b189
Steve <stephenjsweeney@battleforthesolarsystem.com>
2018-05-15 07:37:27 +0100

    GCC compile fix for fadeAmount not being initialized.

--- src/world/world.c
+++ src/world/world.c
@@ -765,6 +765,8 @@ void drawGameOver(void)
 {
 	int fadeAmount;
 	
+	fadeAmount = 0;
+	
 	if (world.gameOverTimer <= -FPS)
 	{
 		fadeAmount = MIN((world.gameOverTimer + FPS) * -1, 128);
@@ -774,7 +776,7 @@ void drawGameOver(void)
 	
 	if (world.gameOverTimer <= -FPS * 2)
 	{
-		blitRect(atlasTexture->texture, SCREEN_WIDTH / 2, 280, &missionFailed->rect, 1);
+		blitRect(atlasTexture->texture, SCREEN_WIDTH / 2, 240, &missionFailed->rect, 1);
 		
 		if (world.gameOverTimer <= -FPS * 3)
 		{
